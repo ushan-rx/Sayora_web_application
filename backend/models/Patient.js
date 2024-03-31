f19896e6c11b863ff2dbc27e27acac55f74e9f62
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-04-01 02:19:24 +0530

    update patient
    remove unnecessary require and change vital structure adding units to each of them

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -2,86 +2,91 @@ import mongoose from "mongoose";
 
 // Define the schema for the regPatient collection
 const patientSchema = new mongoose.Schema({
-    patientId: {
-        type: String,
-        required: true,
-        unique: true
+  patientId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  nic: {
+    type: String,
+    required: true,
+  },
+  allagies: [
+    {
+      type: String,
     },
-    name: {
-        type: String,
-        required: true
+  ],
+  familyHistory: [{ name: { type: String }, relationship: { type: String } }],
+  currentMedications: [
+    {
+      name: { type: String },
+      dosage: { type: String },
+      frequency: { type: String },
     },
-    dob: {
+  ],
+  surgicalProcedures: [{ name: { type: String }, date: { type: Date } }],
+  vitals: [
+    { checkdate: { type: Date } },
+    { height: { type: Number }, unit: { type: String } }, //meke hama ekatama string type eke sub field eka gane dapn unit eka thiyaganna
+    { weight: { type: Number }, unit: { type: String } },
+    { temperature: { type: Number }, unit: { type: String } },
+    { bloodPressure: { type: String } },
+    { pulseRate: { type: Number }, unit: { type: String } },
+    { respiratoryRate: { type: Number }, unit: { type: String } },
+    { oxygenSaturation: { type: Number }, unit: { type: String } },
+  ],
+  treatmentHistory: [
+    {
+      treatment: {
+        type: Schema.Types.ObjectId, // treatment referenced
+        ref: "Treatment",
+      },
+      date: {
         type: Date,
-        required: true
+        required: true,
+      },
     },
-    gender: {
-        type: String,
-        enum: ['Male', 'Female', 'Other'],
-        required: true
+  ],
+
+  dailyQuestions: [
+    {
+      date: { type: Date },
+      questionId: { type: String },
+      mark: { type: Numbere },
     },
-    phone: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    bloodGroup: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    nic: {
-        type: String,
-        required: true
-    },
-    allagies:[
-        {
-            type: String,
-            required: true
-        }
-    ],
-    familyHistory:[
-        {   name:{type: String,required: true},
-            relationship:{type: String,required: true},
-        }
-    ],
-    currentMedications:[
-        {   name:{type: String,required: true},
-            dosage:{type: String,required: true},
-            frequency:{type: String,required: true},
-        }
-    ],
-    surgicalProcedures:[
-        {   name:{type: String,required: true},
-            date:{type: Date,required: true},
-        }
-    ],
-    vitals:[
-        {   checkdate:{type: Date,required: true},
-            height:{type: Number,required: true},
-            weight:{type: Number,required: true},
-            temperature:{type: Number,required: true},
-            bloodPressure:{type: String,required: true},
-            pulseRate:{type: Number,required: true},
-            respiratoryRate:{type: Number,required: true},
-            oxygenSaturation:{type: Number,required: true},
-        }
-    ],
-    dailyQuestions:[
-        {   date:{type: Date,required: true},
-            questionId:{type: String,required: true},
-            mark:{type: Number,required: true},
-        }
-    ],
+  ],
 });
 
 // Create the patient model
-const Patient = mongoose.model('Patient', patientSchema);
+const Patient = mongoose.model("Patient", patientSchema);
 
 module.exports = Patient;

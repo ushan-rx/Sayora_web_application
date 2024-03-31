ee432df05574409a21499684c7812d3d8149d340
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-04-01 02:17:50 +0530

    Update Resultreport.js
    remove unnecessary require and change model name as "Report"

--- backend/models/ResultReports.js
+++ backend/models/ResultReports.js
@@ -2,29 +2,23 @@ import mongoose from "mongoose";
 
 // Define the schema for the result reports model
 const resultReportsSchema = new mongoose.Schema({
-    reportId: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    patientId: {
-        type: String,
-        required: true
-    },
-    testId: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    documentURL: {
-        type: String,
-        required: true
-    }
+  patientId: {
+    type: String,
+    required: true,
+  },
+  testName: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  documentURL: {
+    type: String,
+  },
 });
 
 // Create the result reports model
-const ResultReportsModel = mongoose.model("resultReports", resultReportsSchema);
-module.exports = ResultReportsModel;
+const Report = mongoose.model("resultReports", resultReportsSchema);
+module.exports = Report;

9e31fd9f9e2a435a6969e12f1a16ed0c890659b9
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-04-01 02:15:12 +0530

    Update DailyQuest.js
    remove  unnecessary required

--- backend/models/DailyQuestions.js
+++ backend/models/DailyQuestions.js
@@ -2,27 +2,32 @@ import mongoose from "mongoose";
 
 // Define the schema for the daily questions model
 const dailyQuestionSchema = new mongoose.Schema({
-    questionId: {
-        type: String,
-        required: true,
-        unique: true
+  questionId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  options: [
+    {
+      number: { type: Number, required: true, unique: true },
+      type: String,
+      required: true,
     },
-    question: {
-        type: String,
-        required: true
-    },
-    options: [{
-        number: {type: Number,required: true,unique: true},
-        type: String,
-        required: true
-    }],
-    answer: {
-        optionNumber: {type: Number,required: true},
-        type: String,
-        required: true
-    }
+  ],
+  answer: {
+    optionNumber: { type: Number, required: true },
+    type: String,
+    required: true,
+  },
 });
 
 // Create the daily questions model
-const DailyQuestionsModel = mongoose.model("dailyQuestions", dailyQuestionSchema);
+const DailyQuestionsModel = mongoose.model(
+  "dailyQuestions",
+  dailyQuestionSchema
+);
 model.export = DailyQuestionsModel;

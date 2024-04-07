01be3dad600903964a11f50b38dee69c5a4d3e9e
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-04-07 16:09:52 +0530

    Update ovwerview patient

--- frontend/src/pages/patient/overviewPatient.jsx
+++ frontend/src/pages/patient/overviewPatient.jsx
@@ -1,118 +1,66 @@
-import { Fragment } from 'react'
-import {
-  BriefcaseIcon,
-  CalendarIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  CurrencyDollarIcon,
-  LinkIcon,
-  MapPinIcon,
-  PencilIcon,
-} from '@heroicons/react/20/solid'
-import { Menu, Transition } from '@headlessui/react'
+import React from 'react'
+import { Calendar } from "@/components/ui/calendar"
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
-export default function Example() {
+function overview() {
   return (
-    <div className="lg:flex lg:items-center lg:justify-between">
-      <div className="flex-1 min-w-0">
-        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-          Back End Developer
-        </h2>
-        <div className="flex flex-col mt-1 sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-          <div className="flex items-center mt-2 text-sm text-gray-500">
-            <BriefcaseIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            Full-time
+    <div>
+    <div className="flex flex-col md:flex-row">
+      <div className="p-4 bg-gray-200 md:w-4/5">Column 1</div>
+      <div className="p-4 bg-white md:w-1/4">
+      <h2 className="mb-4 text-xl font-bold">Nearest Treatment</h2>
+        <div className="grid grid-cols-1 gap-4">
+          {/* Square Card 1 */}
+          <div className="px-6 py-4 bg-[#089BAB] border border-gray-300 rounded-2xl">
+            <div className="flex justify-between">
+              <div>
+                <div className="text-center text-white text-m">Aug</div>
+                <div className="text-5xl font-bold text-white">26</div>
+              </div>
+              <div className="text-right">
+                <div className="text-sm text-white">Dr. John Doe</div>
+                <div className="mb-4 text-xs text-white">Skin therepist</div>
+                <div className="p-2 text-lg bg-[#e0eff1] rounded-lg ">9:00 AM</div>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center mt-2 text-sm text-gray-500">
-            <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            Remote
+          {/* Square Card 2 */}
+          <div className="p-4 bg-white border border-gray-300 rounded-2xl">
+            <div className="flex justify-between">
+              <div>
+                <div className="text-sm text-gray-500">Sep</div>
+                <div className="text-4xl font-bold">15</div>
+              </div>
+              <div className="text-right">
+                <div className="text-sm text-gray-700">Dr. Jane Smith</div>
+                <div className="text-lg">2:30 PM</div>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center mt-2 text-sm text-gray-500">
-            <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            $120k &ndash; $140k
-          </div>
-          <div className="flex items-center mt-2 text-sm text-gray-500">
-            <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            Closing on January 9, 2020
+          {/* Square Card 2 */}
+          <div className="p-4 bg-white border border-gray-300 rounded-2xl">
+            <div className="flex justify-between">
+              <div>
+                <div className="text-sm text-gray-500">Sep</div>
+                <div className="text-4xl font-bold">15</div>
+              </div>
+              <div className="text-right">
+                <div className="text-sm text-gray-700">Dr. Jane Smith</div>
+                <div className="text-lg">2:30 PM</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex mt-5 lg:ml-4 lg:mt-0">
-        <span className="hidden sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            <PencilIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-            Edit
-          </button>
-        </span>
-
-        <span className="hidden ml-3 sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            <LinkIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-            View
-          </button>
-        </span>
-
-        <span className="sm:ml-3">
-          <button
-            type="button"
-            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <CheckIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-            Publish
-          </button>
-        </span>
-
-        {/* Dropdown */}
-        <Menu as="div" className="relative ml-3 sm:hidden">
-          <Menu.Button className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-            More
-            <ChevronDownIcon className="-mr-1 ml-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-          </Menu.Button>
-
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                  >
-                    View
-                  </a>
-                )}
-              </Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+      <div className="object-center p-4 bg-white md:w-1/4">
+        <div className='flex justify-center'>
+          <Calendar className="my-4 border rounded-2xl"></Calendar>
+        </div>
+        
       </div>
+    </div>
     </div>
   )
 }
+
+export default overview

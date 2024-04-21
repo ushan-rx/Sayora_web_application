dfb96d68ece315d464a06de25c69fcef08fd5292
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-04-22 01:20:24 +0530

    update requesition route

--- backend/routes/requesition.route.js
+++ backend/routes/requesition.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createRequesition, getAllRequesitions, getRequesitionsByPatient, deleteRequesition } = require('../controllers/requesition.controller');
+const { createRequesition, getAllRequesitions, getRequesitionsByPatient, deleteRequesition, getManyRequesition, updateRequesition } = require('../controllers/requesition.controller');
 
 router.route('/').post(createRequesition).get(getAllRequesitions);      // '/requesition'
-router.route('/:id').get(getRequesitionsByPatient).delete(deleteRequesition);                          // '/requesition/:id'
+router.route('/:id').get(getRequesitionsByPatient).delete(deleteRequesition).put(updateRequesition);                          // '/requesition/:id'
+
+// get all treatments using patient id
+router.route('/patient/:id').get(getManyRequesition);
+
 module.exports = router;
